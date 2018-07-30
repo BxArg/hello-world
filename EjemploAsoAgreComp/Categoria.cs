cc371a70b68378ab3b2a025be7779961d33982e6
Leonardo Giaquinta <lgiaquintaruiz@buxis.com>
2018-07-30 18:31:59 -0300

    prueba

--- EjemploAsoAgreComp/Categoria.cs
+++ EjemploAsoAgreComp/Categoria.cs
@@ -8,6 +8,7 @@ namespace EjemploAsoAgreComp
     class Categoria
     {
         public int Id { get; set; }
-        public string IdCategoria { get; set; }
+        public string IdCategoria { get; }
+        public string IdCategoria2 { get; set; }
     }
 }
